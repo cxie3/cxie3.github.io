ba29019bc4767f32009ad8a1ec08352875ba1145
cxie3 <cxie3@uncc.edu>
2022-05-01 11:46:08 -0400

    13 and 14

---
+++ itis3135/project/scripts/magnify.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('[data-magnify=gallery]').magnify({
+
+    modalWidth: 320,
+    modalHeight: 320
+
+    });
+});
---
+++ itis3135/project/scripts/slideshow.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.slider').bxSlider({
+        auto: true,
+        minSlides: 1,
+        maxSlides: 1,
+        slideWidth: 500,
+        adaptiveHeight: true,
+        captions: true,
+        speed: 1000,
+        pause:2000
+      
+    });
+  });
